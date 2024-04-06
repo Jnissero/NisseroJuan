ff29f00124bfd7033ddf1a48fe545c4deab88947
Jnissero <juanmaria2755@gmail.com>
2024-04-05 21:02:49 -0300

    subo nuevos archivos

---
+++ TP1/fn.js
@@ -0,0 +1,16 @@
+let frutas = ['pera', 'manzana', 'frutilla', 'banana', 'kiwi'];
+
+
+/*console.log($array[1]);
+*/
+
+/*frutas.forEach( fruta => {
+console.log(fruta);
+})*/
+
+for (const fruta of frutas) {
+    if (fruta == 'manzana')
+        console.log(fruta)
+}
+
+
